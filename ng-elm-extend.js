09efbc0670d439d1f995636be9bd4155ea826c4b
Oliver Sourbut <olyis@live.co.uk>
2017-01-18 20:13:32 +0000

    removing debug console lines

--- ng-elm-extend.js
+++ ng-elm-extend.js
@@ -1,4 +1,4 @@
-(function(console, undefined){
+(function(){
     const oldModule = angular.module;
     const $injector = angular.injector(['ng', 'ngElm'], true);
     const ElmService = $injector.get('ElmService');
@@ -6,10 +6,8 @@
         var app = oldModule(...args);
         app.elmController = function(ctrlName, ctrlDef) {
             var translatedCtrlDef = ElmService.translateController(ctrlDef);
-            console.log('registering Elm Controller: ' + ctrlName);
             app.controller(ctrlName, translatedCtrlDef);
         }
-        console.log('returning module: ' + args);
         return app;
     }
-})(console);
+})();
